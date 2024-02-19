eccdae3e1fb442608e73ed7be992ed7a803a798f
puleng <101699697+PulengAlvi@users.noreply.github.com>
2024-02-19 16:29:00 +0200

    Delete pulengtshaba directory

--- pulengtshaba/src/main/java/com/enviro/assessment/grad001/pulengtshaba/controller/ProductController.java
+++
@@ -1,8 +0,0 @@
-package com.enviro.assessment.grad001.pulengtshaba.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ProductController {
-    
-}
--- pulengtshaba/src/main/java/com/enviro/assessment/grad001/pulengtshaba/controller/WithdrawalController.java
+++
@@ -1,5 +0,0 @@
-package com.enviro.assessment.grad001.pulengtshaba.controller;
-
-public class WithdrawalController {
-    
-}
